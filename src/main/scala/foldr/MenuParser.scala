ab29aa0df056c2a69ae5f44380fafa6191be1a12
pierre <pierre@magistry.fr>
2018-06-22 16:24:04 +0200

    tags, titre

--- src/main/scala/foldr/MenuParser.scala
+++ src/main/scala/foldr/MenuParser.scala
@@ -23,12 +23,19 @@ object MenuParser {
     def render(router: Router, idx: String): (Element, List[Element])
   }
 
-  case class Link(label: String, url: String, options: Map[String,String]) extends MenuEntry {
+  case class Link(label: String, url: String, options: Map[String,String], tag: String) extends MenuEntry {
 
     var tabId: Option[String] = None
 
     override def render(router: Router, idx: String): (Element, List[Element]) = {
 
+      val tagDiv = tag.trim match {
+        case "" => None
+        case s => s.split(" ").toList match {
+          case color:: tl => Some(div(cls:=s"ui label $color", tl mkString " "))
+          case _ => None
+        }
+      }
       val reloadButton =
         //button(cls := "ui small icon button",
           i(cls:= "refresh icon").render
@@ -36,6 +43,7 @@ object MenuParser {
       val a = JsDom.all.a(cls := "inverted link item",
         attr("data-tab") := s"tab-$idx",
         reloadButton,
+        tagDiv,
         label
       ).render
       router.registerTab(a, label, s"tab-$idx")
@@ -100,6 +108,12 @@ object MenuParser {
     }
   }
 
+  case class Title(label:String, options: Map[String, String]) extends MenuEntry {
+    override def render(router: Router, idx: String): (Element, List[Element]) = {
+      (div(cls := "ui item", label).render, Nil)
+    }
+  }
+
 
 
 
@@ -125,16 +139,19 @@ object MenuParser {
     val content = req.responseText
     CSVParser.parse(content).foldLeft(List.empty[Option[MenuEntry]]) {case (list,line) =>
       line.map(_.trim) ++ Array("","","") match {
+        case Array(title, "", options, _*) if title.trim != "" =>
+          val optMap = options.split(" ").map(_.split(":", 2)).collect {case Array(k,v) => k -> v} .toMap
+          Some(Title(title,optMap)) :: list
         case Array("","","", _*) => None :: list
         case Array("", label, options, tags, _*) =>
           println(s"dir $label")
           val dir = Directory(label,options.split(" "), Nil)
           Some(dir) :: list
-        case Array(url, label, options, tags, _*) =>
+        case Array(url, label, options, tag, _*) =>
           println(s"link $label")
           val optMap = options.split(" ").map(_.split(":", 2)).collect {case Array(k,v) => k -> v} .toMap
           optMap.get("PDG").foreach(v => graphDataMapping(v) = url)
-          val link = Link(label.trim, url.trim, optMap)
+          val link = Link(label.trim, url.trim, optMap,tag)
           list.headOption.flatten match {
             case Some(Directory(label, options, entries)) => Some(Directory(label, options, entries :+ link)) :: list.tail
             case _ => Some(link) :: list
@@ -162,7 +179,14 @@ object MenuParser {
     val menuItems = elements.map(_._1)
     val tabs = elements.flatMap(_._2)
 
+    // set page title
+    entries
+      .collectFirst {case Title(label,_) => label }
+      .foreach(dom.document.title = _)
+
+
     for(item <- menuItems) {
+
       menuDiv.appendChild(item)
         //div(cls := "ui fluid vertical menu", menuItems).render
     }
